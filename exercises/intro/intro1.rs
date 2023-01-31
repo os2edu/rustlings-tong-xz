8e0d35edf92ff0321559edbbcd3280128dab2f8a
X_Z <52037372+tong-xz@users.noreply.github.com>
2023-01-31 23:14:49 +0800

    1

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -9,7 +9,6 @@
 // when you change one of the lines below! Try adding a `println!` line, or try changing
 // what it outputs in your terminal. Try removing a semicolon and see what happens!
 
-// I AM NOT DONE
 
 fn main() {
     println!("Hello and");
@@ -20,7 +19,6 @@ fn main() {
     println!(r#" | |  | |_| \__ \ |_| | | | | | (_| \__ \ "#);
     println!(r#" |_|   \__,_|___/\__|_|_|_| |_|\__, |___/ "#);
     println!(r#"                               |___/      "#);
-    println!();
     println!("This exercise compiles successfully. The remaining exercises contain a compiler");
     println!("or logic error. The central concept behind Rustlings is to fix these errors and");
     println!("solve the exercises. Good luck!");
